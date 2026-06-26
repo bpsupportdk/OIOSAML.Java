301fba47160f8ca4935472c1ba224d56e554a2a1
Jesper Utoft <jesper.utoft@capgemini.com>
2026-06-26 11:48:16 +0200

    3.2.2-opensaml-5.2.3-cap-8 is opensaml 5.2.3 but with a new SameSiteFilter

--- oiosaml/src/main/java/dk/gov/oio/saml/filter/SameSiteFilter.java
+++ oiosaml/src/main/java/dk/gov/oio/saml/filter/SameSiteFilter.java
@@ -8,104 +8,51 @@ import java.util.Collection;
 
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
-import jakarta.servlet.FilterConfig;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpServletResponseWrapper;
 
 public class SameSiteFilter implements Filter {
     private static final String SAMESITE_COOKIE_HEADER = "Set-Cookie";
     private static final String SAMESITE_ATTRIBITE_NAME = "SameSite";
     private static final String SAMESITE_NONE_VALUE = "None";
 
-    public void init(FilterConfig filterConfig) throws ServletException {
-    }
-
-    public void destroy() {
-    }
-
+    @Override
     public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain) throws IOException, ServletException {
-        if (response instanceof HttpServletResponse) {
-            chain.doFilter(request, new SameSiteWrapper((HttpServletResponse) response));
-        }
-        else {
+        if (response instanceof HttpServletResponse httpServletResponse) {
+            patchSameSiteCookies(httpServletResponse);
             chain.doFilter(request, response);
         }
+
+        chain.doFilter(request, response);
     }
-    
-    private class SameSiteWrapper extends HttpServletResponseWrapper {
-        private HttpServletResponse response;
-        
-        public SameSiteWrapper(HttpServletResponse resp) {
-            super(resp);
 
-            response = resp;
+    private void patchSameSiteCookies(HttpServletResponse response) {
+        Collection<String> headers = response.getHeaders(SAMESITE_COOKIE_HEADER);
+        if (headers == null || headers.isEmpty()) {
+            return;
         }
-        
-        @Override
-        public void sendError(int sc) throws IOException {
-            fixSameSiteCookies();
 
-            super.sendError(sc);
-        }
-        
-        @Override
-        public PrintWriter getWriter() throws IOException {
-            fixSameSiteCookies();
-
-            return super.getWriter();
-        }
-        
-        @Override
-        public void sendError(int sc, String msg) throws IOException {
-            fixSameSiteCookies();
-
-            super.sendError(sc, msg);
-        }
-        
-        @Override
-        public void sendRedirect(String location) throws IOException {
-            fixSameSiteCookies();
-
-            super.sendRedirect(location);
-        }
-        
-        @Override
-        public ServletOutputStream getOutputStream() throws IOException {
-            fixSameSiteCookies();
-
-            return super.getOutputStream();
-        }
-        
-        private void fixSameSiteCookies() {
-            Collection<String> headers = response.getHeaders(SAMESITE_COOKIE_HEADER);
-            if (headers == null || headers.size() == 0) {
-                return;
+        boolean firstCookie = true;
+        for (String header : headers) {
+            if (StringUtil.isEmpty(header)) {
+                continue;
             }
 
-            boolean firstCookie = true;
-            for (String header : headers) {
-                if (StringUtil.isEmpty(header)) {
-                    continue;
-                }
-
-                if (!header.contains(SAMESITE_ATTRIBITE_NAME)) {
-                    header = header + ";" + SAMESITE_ATTRIBITE_NAME + "=" + SAMESITE_NONE_VALUE;                
-                } 
-
-                // overwrite existing cookies on first run, then append the new ones
-                if (firstCookie) {
-                    response.setHeader(SAMESITE_COOKIE_HEADER, header);
-                }
-                else {
-                    response.addHeader(SAMESITE_COOKIE_HEADER, header);
-                }
-
-                firstCookie = false;
+            if (!header.contains(SAMESITE_ATTRIBITE_NAME)) {
+                header = header + ";" + SAMESITE_ATTRIBITE_NAME + "=" + SAMESITE_NONE_VALUE;
             }
+
+            // overwrite existing cookies on first run, then append the new ones
+            if (firstCookie) {
+                response.setHeader(SAMESITE_COOKIE_HEADER, header);
+            }
+            else {
+                response.addHeader(SAMESITE_COOKIE_HEADER, header);
+            }
+
+            firstCookie = false;
         }
     }
 }
